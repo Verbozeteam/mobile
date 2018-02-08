89238263e66e1f6e4f2e8c62a3dd07fc4ec46cd5
Mohammed M. Fituri <mfituri94@gmail.com>
2018-02-08 17:53:17 +0300

    status bar is now white

--- VerbozeMobile.js
+++ VerbozeMobile.js
@@ -1,6 +1,7 @@
 /* @flow */
 
 import React, { Component } from 'react';
+import { StatusBar } from 'react-native';
 import Navigation from './navigation/Navigation';
 
 type PropsType = {};
@@ -11,6 +12,11 @@ export default class VerbozeMobile extends Component<PropsType, StateType> {
   static defaultProps = {
 
   };
+
+  componentWillMount() {
+    /* set status bar color to light */
+    StatusBar.setBarStyle('light-content', true);
+  }
 
   render() {
     return (
