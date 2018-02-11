6aa5ee707e5b9d1c0430e4db184ed9661f7ea17e
Mohammed M. Fituri <mfituri94@gmail.com>
2018-02-11 13:56:00 +0300

    added flowtypes.js and initial styles.js

---
+++ constants/styles.js
@@ -0,0 +1,21 @@
+/* @flow */
+
+export const Colors = {
+  red: '#BA3737',
+  white: '#FFFFFF',
+  gray: '#707070',
+  light_gray: '#D8D8D8',
+  black: '#000000',
+};
+
+export const Gradients = {
+  background_dark: ['#1E1E1E', '#080808']
+};
+
+export const TypeFaces = {
+  header: {
+    fontSize: 40,
+    fontWeight: 'bold',
+    color: Colors.white
+  }
+};
